2284168305dbcfef854d490dfd5e5e92f2e9981b
leonpo <593037503@qq.com>
2025-10-11 11:56:08 +0800

    init pom

---
+++ src/main/java/com/datasync/DataMigrationDemoApplication.java
@@ -0,0 +1,18 @@
+package com.datasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 苍穹Web服务主启动类
+ */
+@SpringBootApplication
+public class DataMigrationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataMigrationDemoApplication.class, args);
+    }
+
+}
+    
